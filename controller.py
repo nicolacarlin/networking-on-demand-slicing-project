2162dddfff2d7a1576c160132ca1a0587b4cb0a8
Edoardo Mich <71270665+edoardomich@users.noreply.github.com>
2024-06-06 14:29:56 +0000

    Add direct support to OpenFlow13 and remove usage of TCLink

--- controller.py
+++ controller.py
@@ -2,7 +2,7 @@ import json
 import requests
 import time 
 from webob import Response
-from ryu.controller.handler import MAIN_DISPATCHER, set_ev_cls
+from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER, set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib import dpid as dpid_lib
 from ryu.lib import stplib
@@ -10,6 +10,7 @@ from ryu.lib.packet import packet, ethernet, ether_types
 
 from ryu.app.wsgi import ControllerBase, WSGIApplication, route
 from ryu.controller import dpset
+from ryu.controller import ofp_event
 from ryu.app.simple_switch_13 import SimpleSwitch13
 
 switch_instance_name = 'switch_api_app'
@@ -182,6 +183,24 @@ class Controller(SimpleSwitch13):
         self.logger.debug("[dpid=%s][port=%d] state=%s",
                           dpid_str, ev.port_no, of_state[ev.port_state])
 
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # install table-miss flow entry
+        #
+        # We specify NO BUFFER to max_len of the output action due to
+        # OVS bug. At this moment, if we specify a lesser number, e.g.,
+        # 128, OVS will send Packet-In with invalid buffer_id and
+        # truncated packet data. In that case, we cannot output packets
+        # correctly.  The bug has been fixed in OVS v2.1.0.
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+        
     def restart_stp(self):
         for bridge in self.stp.bridge_list.values():
             try:
